deba39e18f21962ba4b721edd74120f1da7f618a
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-02 12:36:50 -0500

    question 2 added, made keys lower case

--- src/Components/ArrayQuestions/arrayMethods.tsx
+++ src/Components/ArrayQuestions/arrayMethods.tsx
@@ -1,12 +1,22 @@
 const arrayMethods = () => {
   const questionBank = [
-    {difficulty: 'Medium',
-    question: 'What method is used to method create a new array populated with the results of calling a provided function on every element in the calling array.',
-    A: 'Array.map()',
-    B: 'Array.forEach()',
-    C: 'Array.every()'
-    },
 
+    {
+      difficulty: 'Medium',
+    question: 'What method is used to method create a new array populated with the results of calling a provided function on every element in the calling array.',
+    answer: 'a',
+    a: 'Array.map()',
+    b: 'Array.forEach()',
+    c: 'Array.every()',
+    },
+    {
+      difficulty: 'Medium',
+      question: 'What method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.',
+      answer: 'c',
+      a:'Array.findIndex()',
+      b:'Array.findLast()',
+      c:"Array.find()"
+    }
   ]
 
   return (
