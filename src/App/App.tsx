2563b9da4085f04b7020c9836bfbacfce321e42d
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-02 12:25:08 -0500

    arrayMethods component init

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1 style={{'fontSize':300}}>hello</h1>
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import App from './App/App';
 // import reportWebVitals from './reportWebVitals';
 import './Sass/main.scss';
 
