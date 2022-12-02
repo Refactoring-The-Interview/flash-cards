5346afb6878efc5df64e7e26060e41bd3eb08e1c
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-02 12:32:25 -0500

    meduim question created

--- src/Components/ArrayQuestions/arrayMethods.tsx
+++ src/Components/ArrayQuestions/arrayMethods.tsx
@@ -1,7 +1,19 @@
 const arrayMethods = () => {
+  const questionBank = [
+    {difficulty: 'Medium',
+    question: 'What method is used to method create a new array populated with the results of calling a provided function on every element in the calling array.',
+    A: 'Array.map()',
+    B: 'Array.forEach()',
+    C: 'Array.every()'
+    },
 
+  ]
 
-  return null
+  return (
+    <div>
+
+    </div>
+  )
 }
 
 export default arrayMethods;
