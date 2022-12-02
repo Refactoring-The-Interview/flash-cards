ee9b2751846652497e61dff6e5dcaa4403797cfb
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-02 16:00:09 -0500

    data fix for #6  resolve

--- src/Components/ArrayQuestions/arrayMethods.tsx
+++ src/Components/ArrayQuestions/arrayMethods.tsx
@@ -8,41 +8,32 @@ const arrayMethods = () => {
       difficulty: 'Medium',
       question: 'What method is used to method create a new array populated with the results of calling a provided function on every element in the calling array.',
       answer: 'a',
-      a: 'Array.map()',
-      b: 'Array.forEach()',
-      c: 'Array.every()',
+      answers: ['Array.map()','Array.forEach()','Array.every()']
     },
     {
       difficulty: 'Medium',
       question: 'What method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.',
-      answer: 'c',
-      a:'Array.findIndex()',
-      b:'Array.findLast()',
-      c:"Array.find()"
+      answer: 'Array.find()',
+      answers: ['Array.findIndex()','Array.findLast()','Array.find()']
+
     },
     {
       difficulty: 'Medium',
       question: 'what method executes a provided function once for each array element.',
-      answer: 'b',
-      a: 'Array.map()',
-      b: 'Array.forEach()',
-      c: 'Array.every()',
+      answer: 'Array.forEach()',
+      answers: ['Array.map()','Array.forEach()','Array.every()']
     },
     {
       difficulty: 'Medium',
       question: " What method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. It doesn't modify the array.",
-      answer: 'a',
-      a:'Array.some()',
-      b:'Array.filter',
-      c:'Array.forEach()'
+      answer: 'Array.some()',
+      answers: ['Array.some()','Array.filter','Array.forEach()']
     },
     {
       difficulty: 'Medium',
       question: "What method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.",
       answer: 'Array.flat()',
-      a:'Array.flat()',
-      b:'Array.flatMap()',
-      c:'Array.concat()'
+      answers: ['Array.flat()','Array.flatMap()','Array.concat()']
     }
 
   ]
@@ -56,7 +47,7 @@ const arrayMethods = () => {
       </div>
 
       <ul className='card__list'>
-        <li className='card__item'>
+        {/* <li className='card__item'>
           (A): {a}
         </li>
         <li className='card__item'>
@@ -64,7 +55,7 @@ const arrayMethods = () => {
         </li>
         <li className='card__item'>
           (C): {c}
-        </li>
+        </li> */}
       </ul>
 
     </div>
