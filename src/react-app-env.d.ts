2d4c6a82324fe4434dc0e6421c8af252d5a42bc9
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-02 11:55:09 -0500

    env declorations for ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.mp4";
+declare module "*.mp3";
+declare module "*.webm";
+declare module "*.pdf";
+declare module "*.gltf";
+declare module "*.svg";
