4b53d4a5b20079ddbdd28ad5f391fd7d7975b049
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-05 15:22:52 -0500

    html for from base made

--- src/Components/FashCard/FlashCard.tsx
+++ src/Components/FashCard/FlashCard.tsx
@@ -36,15 +36,52 @@ const FlashCard = () => {
           onClick={(e)=> {setAddQuestion(!addQuestion)}}>Add</button>
         {
           addQuestion && (
-            <form className="form">
+            <form className="form" onSubmit={(e) => {
+              e.preventDefault()
+              console.log(e.target)
+
+              }}>
               <div className="form__difficulty">
-              <label htmlFor="difficultyFormControlSelect1">difficulty select: </label>
-              <select className="form__difficulty-options" id="difficultyFormControlSelect1">
+              <label htmlFor="difficulty-options">difficulty select: </label>
+              <select
+                className="form__difficulty-options"
+                id="difficulty-options"
+                value={newQuestion.difficulty}
+              >
                 <option>Easy</option>
                 <option>Medium</option>
                 <option>Hard</option>
               </select>
             </div>
+
+            <div className="form__question">
+             <label htmlFor="questionTextArea">Question</label>
+              <textarea
+               className="form__question__descriptor form-control"
+               id="questionTextArea"
+               value={newQuestion.question}
+               ></textarea>
+            </div>
+
+            <div className="form__answers">
+             <label htmlFor="answersTextArea">Answers</label>
+              <textarea
+               className="form__answers__descriptor form-control"
+               id="answersTextArea"
+               value={newQuestion.answers}
+               ></textarea>
+            </div>
+
+            <div className="form__answer">
+             <label htmlFor="answerTextArea">Correct Answer</label>
+              <textarea
+               className="form__answer__descriptor form-control"
+               id="answerTextArea"
+               value={newQuestion.answers}
+               ></textarea>
+            </div>
+
+            <button type="submit" className="btn btn-danger">Submit</button>
             </form>
           )
         }
