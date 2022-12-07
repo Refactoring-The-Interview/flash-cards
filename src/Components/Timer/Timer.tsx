2b6267ff5fe7d8a4f5a93d06f5fa7121e8002f20
Alex Hamstra <ahamstra@anduril.com>
2022-12-07 11:07:56 -0500

    Run prettier on every file

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -1,53 +1,52 @@
-import React, {useState} from "react";
-import { useTimer } from 'react-timer-hook';
-import './TimerS.scss';
-import iconSprite from '../../Assets/icons/sprite.svg';
+import React, { useState } from "react";
+import { useTimer } from "react-timer-hook";
+import "./TimerS.scss";
+import iconSprite from "../../Assets/icons/sprite.svg";
 
 const Timer = (expiryTimestamp: any) => {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 300);
 
-  const {
-    seconds,
-    minutes,
-    start,
-    pause,
-    restart,
-  } = useTimer({ expiryTimestamp: time , autoStart: true });
-  const [isPause, setIsPause] = useState (false)
+  const { seconds, minutes, start, pause, restart } = useTimer({
+    expiryTimestamp: time,
+    autoStart: true,
+  });
+  const [isPause, setIsPause] = useState(false);
 
   return (
     <div className="timer">
       <svg className="timer__icon-stopwatch">
         <use xlinkHref={`${iconSprite}#icon-stopwatch`}> </use>
       </svg>
-      <div className="timer__display" >
-       <span>{minutes}</span>:<span>{seconds}</span>
+      <div className="timer__display">
+        <span>{minutes}</span>:<span>{seconds}</span>
       </div>
-      {
-        !isPause && (
-          <button
-          type="button" className="btn timer__btn btn-outline-danger"
+      {!isPause && (
+        <button
+          type="button"
+          className="btn timer__btn btn-outline-danger"
           onClick={() => {
-            pause()
-            setIsPause(true)
-          }}>Pause</button>
-        )
-      }
-      {
-        isPause && (
-          <button
-          type="button" className="btn timer__btn btn-outline-success"
-            onClick={() => {
-              start();
-              setIsPause(false)
-            }}
-          >Start
-          </button>
-        )
-      }
+            pause();
+            setIsPause(true);
+          }}
+        >
+          Pause
+        </button>
+      )}
+      {isPause && (
+        <button
+          type="button"
+          className="btn timer__btn btn-outline-success"
+          onClick={() => {
+            start();
+            setIsPause(false);
+          }}
+        >
+          Start
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default Timer;
