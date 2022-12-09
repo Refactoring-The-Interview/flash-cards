8c07e0b7d7f951c2d9341bb70da82dbf44f4938a
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-09 11:25:08 -0500

    prettier update for spacing 2->4 spaces

--- src/Components/ArrayQuestions/ArrayMethods.tsx
+++ src/Components/ArrayQuestions/ArrayMethods.tsx
@@ -3,40 +3,40 @@ import "./ArrayMethodsS.scss";
 import { Question } from "../store/types";
 
 interface Props {
-  cardQuestion: Question;
+    cardQuestion: Question;
 }
 
 export const ArrayMethods = ({ cardQuestion }: Props) => {
-  const correctCss = "btn btn-success";
-  const incorrectCss = "btn btn-danger";
-  const defaultCss = "btn btn-outline-info";
-  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-  const { question, answer, answers } = cardQuestion;
-  const isCorrect = selectedAnswer === answer;
-  const selectedCss = isCorrect ? correctCss : incorrectCss;
+    const correctCss = "btn btn-success";
+    const incorrectCss = "btn btn-danger";
+    const defaultCss = "btn btn-outline-info";
+    const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+    const { question, answer, answers } = cardQuestion;
+    const isCorrect = selectedAnswer === answer;
+    const selectedCss = isCorrect ? correctCss : incorrectCss;
 
-  return (
-    <div className="ArrayMethods">
-      <div className="description">{question}</div>
+    return (
+        <div className="ArrayMethods">
+            <div className="description">{question}</div>
 
-      <ul className="list">
-        {answers.map((item: string, index: number) => {
-          return (
-            <button
-              type="button"
-              className={`btn-item ${
-                selectedAnswer ? selectedCss : defaultCss
-              }`}
-              key={index}
-              onClick={(e) => {
-                setSelectedAnswer(item);
-              }}
-            >
-              {item}
-            </button>
-          );
-        })}
-      </ul>
-    </div>
-  );
+            <ul className="list">
+                {answers.map((item: string, index: number) => {
+                    return (
+                        <button
+                            type="button"
+                            className={`btn-item ${
+                                selectedAnswer ? selectedCss : defaultCss
+                            }`}
+                            key={index}
+                            onClick={(e) => {
+                                setSelectedAnswer(item);
+                            }}
+                        >
+                            {item}
+                        </button>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
--- src/Components/QuestionRandomizer/randomizer.ts
+++ src/Components/QuestionRandomizer/randomizer.ts
@@ -1,6 +1,6 @@
 export const randomizer = (array: any) => {
-  const randomElement = Math.floor(Math.random() * array.length);
-  const result = array[randomElement];
+    const randomElement = Math.floor(Math.random() * array.length);
+    const result = array[randomElement];
 
-  return result;
+    return result;
 };
