4d806643adfd0fe5063137a43cece3dab46d81b0
sbrandon0432 <sbrandon0432@yahoo.com>
2022-12-19 12:22:02 -0500

    code edditor

--- src/Components/IDE/IDE.tsx
+++ src/Components/IDE/IDE.tsx
@@ -1,5 +1,6 @@
 import CodeEditor from "@uiw/react-textarea-code-editor";
 import { useState } from "react";
+import "./IDES.scss";
 
 export const IDE = () => {
     const [code, setCode] = useState(
@@ -8,15 +9,17 @@ export const IDE = () => {
     return (
         <CodeEditor
             value={code}
+            className="IDE"
             language="js"
             placeholder="Please enter JS code."
             onChange={(evn) => setCode(evn.target.value)}
             padding={15}
             style={{
                 fontSize: 12,
-                backgroundColor: "#f5f5f5",
+                backgroundColor: "#000",
                 fontFamily:
                     "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                color: "orange",
             }}
         />
     );
