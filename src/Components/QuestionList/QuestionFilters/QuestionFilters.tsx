9c2dcc5b6a1cb6e47683991d1b64b16815a8690c
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-16 13:23:42 -0500

    filter search bar created

--- src/Components/QuestionList/QuestionFilters/QuestionFilters.tsx
+++ src/Components/QuestionList/QuestionFilters/QuestionFilters.tsx
@@ -8,17 +8,22 @@ export const QuestionFilters = () => {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Filters</Accordion.Header>
                     <Accordion.Body>
-                        <Form.Select
-                            className="select-bar"
-                            aria-label="Default select example"
-                            onChange={(e) => {
-                                // setQuestionType(e.target.value);
-                            }}
-                        >
-                            <option value="Js">Default Filter</option>
-                            <option value="array">Array</option>
-                            <option value="object">Objects</option>
-                        </Form.Select>
+                        <Form>
+                            <Form.Group className="mb-3">
+                                <Form.Control type="s" placeholder="Search" />
+                            </Form.Group>
+                            <Form.Select
+                                className="select-bar"
+                                aria-label="Default select example"
+                                onChange={(e) => {
+                                    // setQuestionType(e.target.value);
+                                }}
+                            >
+                                <option value="Js">All</option>
+                                <option value="array">Array</option>
+                                <option value="object">Objects</option>
+                            </Form.Select>
+                        </Form>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
