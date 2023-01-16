331632d12539d54eea87f6187aec5b9532e67a90
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-16 13:18:37 -0500

    accordian filters with drop down filter by type for arrays and objects

--- src/Components/QuestionList/QuestionFilters/QuestionFilters.tsx
+++ src/Components/QuestionList/QuestionFilters/QuestionFilters.tsx
@@ -4,18 +4,23 @@ import Form from "react-bootstrap/Form";
 export const QuestionFilters = () => {
     return (
         <div>
-            <Accordion>
-                <Form.Select
-                    className="select-bar"
-                    aria-label="Default select example"
-                    onChange={(e) => {
-                        // setQuestionType(e.target.value);
-                    }}
-                >
-                    <option value="Js">Default Filter</option>
-                    <option value="array">Array</option>
-                    <option value="object">Objects</option>
-                </Form.Select>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Filters</Accordion.Header>
+                    <Accordion.Body>
+                        <Form.Select
+                            className="select-bar"
+                            aria-label="Default select example"
+                            onChange={(e) => {
+                                // setQuestionType(e.target.value);
+                            }}
+                        >
+                            <option value="Js">Default Filter</option>
+                            <option value="array">Array</option>
+                            <option value="object">Objects</option>
+                        </Form.Select>
+                    </Accordion.Body>
+                </Accordion.Item>
             </Accordion>
         </div>
     );
