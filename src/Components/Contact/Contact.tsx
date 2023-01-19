b761f4a898adc873ffcdd29bdf7d1a0ccb9bc1d9
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-19 18:05:54 -0500

    basic contact info

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,3 +1,12 @@
+import { ListGroup } from "react-bootstrap";
+
 export const Contact = () => {
-    return <></>;
+    return (
+        <ListGroup>
+            <ListGroup.Item variant="primary">Contact Info</ListGroup.Item>
+            <ListGroup.Item>Email: SamBranodn0432@yahoo.com</ListGroup.Item>
+            <ListGroup.Item>Phone: +1(510)-856-6441</ListGroup.Item>
+            <ListGroup.Item>Location: Knoxville, TN</ListGroup.Item>
+        </ListGroup>
+    );
 };
