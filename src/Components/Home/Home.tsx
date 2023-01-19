6c8c38f809173b2701a7eac4aac3867a7d1cdbe7
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-19 17:02:11 -0500

    addition to the about' :

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -18,16 +18,23 @@ export const Home = () => {
                             Engineers in the Interview process, by providing
                             questions that have real world applications for the
                             job, and then the tools to use the theoretical
-                            knowledge in an Interview style environment. We seek
+                            knowledge in an Interview style environment.
                         </Card.Text>
                         <Card.Text>
-                            to fill the gap that Interview prep providers have.
-                            A lack of structure in how we guild you thought
-                            Interview prep. All to often there are only the
-                            theoretical Interview questions that have no real
-                            impact on the job. or having you build out a
-                            Component or algorithm, but then failing to show the
-                            applications of how to use that in a real world job.
+                            We seek to fill the gap that Interview prep
+                            providers have. A lack of structure in how they
+                            guild you thought Interview prep. All to often there
+                            are only the theoretical Interview questions that
+                            have no real impact on the job. or having you build
+                            out a Component or algorithm, but then failing to
+                            show the applications of how to use that in a real
+                            world job. We provide Questions that are theoretical
+                            but also how that knowledge is applicable to the
+                            position, Then the environment to use the knowledge
+                            in a job like code environment. We invite you to
+                            join our community and refactor how you Interview
+                            prep, but also how you look to solve a problem and
+                            broaden your tools to solve the problem.
                         </Card.Text>
                     </CardGroup>
                 </Card.Body>
