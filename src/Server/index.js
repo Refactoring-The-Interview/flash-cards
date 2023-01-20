7e017c1145167d1fe7d322785015bebeb4f3cbf1
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-20 17:00:13 -0500

    server boiler palte server and simple fetch

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const path = require("path");
 
-app.get("/", (req: any, res: any) => {
-    res.send("Hello World!");
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../public", "index.html"));
+});
+app.get("/server", (req: any, res: any) => {
+    res.send("Hello World! from server");
 });
 
 app.listen(port, () => {
