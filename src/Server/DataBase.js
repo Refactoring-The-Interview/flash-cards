fa4297d4331bc458a2390729e5fe2641621af77a
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-23 11:27:27 -0500

    db connection to the server

--- src/Server/DataBase.js
+++ src/Server/DataBase.js
@@ -1,2 +1,20 @@
 const JSONdb = require("simple-json-db");
-const db = new JSONdb("/path/to/your/storage.json");
+const db = new JSONdb("./storage.json");
+
+const dbGet = (key) => {
+    db.set("1", "hello world");
+    console.log("in db");
+    return db.get(key, (err, data) => {
+        if (err) {
+            // eslint-disable-next-line no-undef
+            callback(err);
+        } else {
+            // eslint-disable-next-line no-undef
+            callback(null, data);
+        }
+    });
+};
+
+module.exports = {
+    dbGet,
+};
