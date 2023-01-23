5b3e8aa780b9fc3e25cfb565bbf3c1265bd74f10
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-23 14:15:35 -0500

    server and client are running and talknig but wont exchange info

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,11 +1,12 @@
 const { dbGet } = require("./DataBase");
-
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    dbGet(req.query, (data, error) => {
+    console.log(req.query);
+    dbGet("1", (data, error) => {
         if (error) {
             res.status(400).send(error);
         } else {
