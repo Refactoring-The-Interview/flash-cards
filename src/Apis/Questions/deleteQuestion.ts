34b065ddc2f4f3cb69c2607c31f41de3d9801e0b
sbrandon0432 <sbrandon0432@yahoo.com>
2023-01-27 16:36:16 -0500

    code clean up

--- src/Apis/Questions/deleteQuestion.ts
+++ src/Apis/Questions/deleteQuestion.ts
@@ -2,7 +2,5 @@ import { API, Question } from "../types";
 import { deleteItem } from "../utils";
 
 export const deleteQuestion = (question: Question): Promise<Question[]> => {
-    // console.log(question, "quesitons");
-    // works here
     return deleteItem(API.deleteItem, question);
 };
