63c531048ee68b3bbcd16f2da20a6af46bcddceb
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-01 12:48:50 -0500

    user profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,3 +1,52 @@
+import { Button, Card, Figure, Modal } from "react-bootstrap";
+import { StorageKey, useLocalStorage } from "../LocalStorage/LocalStorage";
+import { ConfirmButton } from "../ConfirmButton/ConfirmButton";
+import { UserInfoEditForm } from "./UserInfoEditForm/UserInfoEditForm";
+import { useState } from "react";
+import { userInfo } from "../../Apis/types";
+
 export const Profile = () => {
-    return null;
+    const [userInfo, setUserInfo] = useLocalStorage(StorageKey.userInfo, {
+        email: "",
+        password: "",
+    });
+
+    const [newUserInfo, setNewUserInfo] = useState<userInfo>(userInfo);
+
+    return (
+        <>
+            <Card>
+                <Figure>
+                    <Figure.Image
+                        width={171}
+                        height={180}
+                        alt="171x180"
+                        src="holder.js/171x180"
+                    />
+                </Figure>
+                <Card.Body>
+                    <Card.Text>Email: {userInfo.email}</Card.Text>
+
+                    <Card.Text>Password: {userInfo.password}</Card.Text>
+                </Card.Body>
+
+                <ConfirmButton
+                    variant="outline-primary"
+                    onConfirm={() => {
+                        setUserInfo(newUserInfo);
+                    }}
+                    modalBody={
+                        <>
+                            <Modal.Title>Edit User Info</Modal.Title>
+                            <UserInfoEditForm
+                                newUserInfo={newUserInfo}
+                                setNewUserInfo={setNewUserInfo}
+                            />
+                        </>
+                    }
+                    children={<>Edit User Info</>}
+                ></ConfirmButton>
+            </Card>
+        </>
+    );
 };
