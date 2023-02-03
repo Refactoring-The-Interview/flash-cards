a274ed3b431685b28fda1963c911a6084ab2a05c
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-02 19:37:13 -0500

    quesiton filter redo 2

--- src/Components/QuestionList/QuestionFilter/FilterFrom/FilterForm.tsx
+++ src/Components/QuestionList/QuestionFilter/FilterFrom/FilterForm.tsx
@@ -1,3 +1,56 @@
-export const dummy = () => {
-    return null;
+import { Form, Popover } from "react-bootstrap";
+import { Difficulty, FilterSetting } from "../../../../Apis/types";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { DifficultySelect } from "../DifficultySelect/DifficultySelect";
+import { TypeSelect } from "../TypeSelect/TypeSelect";
+import { Checkbox } from "../CheckBox/CheckBox";
+
+interface Props {
+    filterSettings: FilterSetting;
+    setFilterSettings(filterSettings: FilterSetting): void;
+}
+
+export const FilterFrom = ({ filterSettings, setFilterSettings }: Props) => {
+    const { name, difficulty, type, hideCorrect } = filterSettings;
+
+    return (
+        <Form>
+            <SearchBar
+                value={name}
+                setValue={(name) => {
+                    setFilterSettings({
+                        ...filterSettings,
+                        name,
+                    });
+                }}
+            />
+            <DifficultySelect
+                value={difficulty}
+                setValue={(difficulty) => {
+                    setFilterSettings({
+                        ...filterSettings,
+                        difficulty,
+                    });
+                }}
+            />
+            <TypeSelect
+                value={type}
+                setValue={(type) => {
+                    setFilterSettings({
+                        ...filterSettings,
+                        type,
+                    });
+                }}
+            />
+            <Checkbox
+                value={hideCorrect}
+                setValue={(hideCorrect) => {
+                    setFilterSettings({
+                        ...filterSettings,
+                        hideCorrect,
+                    });
+                }}
+            />
+        </Form>
+    );
 };
--- src/Components/QuestionList/QuestionFilter/QuestionFilters.tsx
+++ src/Components/QuestionList/QuestionFilter/QuestionFilters.tsx
@@ -1,100 +1,41 @@
-import Accordion from "react-bootstrap/Accordion";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./QuestionFiltersS.scss";
+import { Button, OverlayTrigger, Popover } from "react-bootstrap";
+import { FilterFrom } from "./FilterFrom/FilterForm";
 import { FilterSetting, filterSettingsDefault } from "../../../Apis/types";
-import { DifficultySelect } from "./DifficultySelect/DifficultySelect";
-import { TypeSelect } from "./TypeSelect/TypeSelect";
-import { SearchBar } from "./SearchBar/SearchBar";
-import { Checkbox } from "./CheckBox/CheckBox";
+import "./QuestionFiltersS.scss";
 
 interface Props {
-    setFilterSettings(filterSettingObject: FilterSetting): void;
     filterSettings: FilterSetting;
+    setFilterSettings(filterSettings: FilterSetting): void;
 }
 
 export const QuestionFilters = ({
     filterSettings,
     setFilterSettings,
 }: Props) => {
-    const formReset = () => {
-        setFilterSettings(filterSettingsDefault);
-    };
+    const test = (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">Settings</Popover.Header>
+            <Popover.Body>
+                <FilterFrom
+                    filterSettings={filterSettings}
+                    setFilterSettings={setFilterSettings}
+                />
+                <Button
+                    onClick={() => {
+                        setFilterSettings(filterSettingsDefault);
+                    }}
+                >
+                    Reset Form
+                </Button>
+            </Popover.Body>
+        </Popover>
+    );
 
     return (
         <div className="QuestionFilters">
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Filters</Accordion.Header>
-                    <Accordion.Body>
-                        <Form
-                            onSubmit={(e) => {
-                                e.preventDefault();
-                                const { difficulty, type, name, hideCorrect } =
-                                    filterSettings;
-                                setFilterSettings({
-                                    type: type,
-                                    name: name,
-                                    hideCorrect: hideCorrect,
-                                    difficulty: difficulty,
-                                });
-                            }}
-                        >
-                            <SearchBar
-                                value={filterSettings.name}
-                                setValue={(name) => {
-                                    setFilterSettings({
-                                        ...filterSettings,
-                                        name,
-                                    });
-                                }}
-                            />
-
-                            <TypeSelect
-                                value={filterSettings.type}
-                                setValue={(type) => {
-                                    setFilterSettings({
-                                        ...filterSettings,
-                                        type,
-                                    });
-                                }}
-                            />
-                            <DifficultySelect
-                                value={filterSettings.difficulty}
-                                setValue={(difficulty) =>
-                                    setFilterSettings({
-                                        ...filterSettings,
-                                        difficulty,
-                                    })
-                                }
-                            />
-                            <Checkbox
-                                value={filterSettings.hideCorrect}
-                                setValue={(hideCorrect) => {
-                                    setFilterSettings({
-                                        ...filterSettings,
-                                        hideCorrect,
-                                    });
-                                }}
-                            />
-
-                            <Form.Group className="buttons">
-                                <Button variant="primary" type="submit">
-                                    filter
-                                </Button>
-
-                                <Button
-                                    variant="primary"
-                                    type="button"
-                                    onClick={formReset}
-                                >
-                                    Reset
-                                </Button>
-                            </Form.Group>
-                        </Form>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+            <OverlayTrigger trigger="click" placement="bottom" overlay={test}>
+                <Button variant="primary">Filter Settings</Button>
+            </OverlayTrigger>
         </div>
     );
 };
