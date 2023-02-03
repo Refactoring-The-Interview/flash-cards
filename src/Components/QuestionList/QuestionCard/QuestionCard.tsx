6c6f3ffde3007093c8a3232f2731efff4f4f2c95
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-03 16:55:48 -0500

    final redo

--- src/Components/QuestionList/QuestionCard/QuestionCard.tsx
+++ src/Components/QuestionList/QuestionCard/QuestionCard.tsx
@@ -1,32 +1,30 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./QuestionCardS.scss";
 import { QuestionTags } from "../QuestionTags/QuestionTags";
 import { DeleteButton } from "../QuestionCards/DeleteButton/DeleteButton";
 import { Question } from "../../../Apis/types";
+import { Badge } from "react-bootstrap";
 
-interface CardProps {
+interface QuestionCardProps {
     question: Question;
     showDelete?: boolean;
 }
 
-export const QuestionCard = ({ question, showDelete }: CardProps) => {
+export const QuestionCard = ({ question, showDelete }: QuestionCardProps) => {
     const { answer, difficulty, tags, correct } = question;
 
-    let answeredColor = correct ? "outline-success" : "outline-danger";
+    const variation = correct ? "success" : "danger";
 
     return (
-        <div className="QuestionCard">
-            <Card className="listItem">
+        <div>
+            <Card>
                 <Card.Header>
-                    {showDelete && <DeleteButton question={question} />}
-                    <Card.Text className="listItem-text">{answer}</Card.Text>
-                    <span className={`difficulty ${difficulty}`}></span>
+                    <Card.Text>{answer}</Card.Text>
+                    <div className={`difficulty ${difficulty}`}></div>
                 </Card.Header>
                 <Card.Body>
-                    <Button variant={`${answeredColor}`} disabled>
-                        Answered
-                    </Button>
+                    {showDelete && <DeleteButton question={question} />}
+                    <Badge bg={variation}>answered</Badge>
                     <QuestionTags tags={tags} />
                 </Card.Body>
             </Card>
