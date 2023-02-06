4a62824f56568f8cbc86e7e1faf12bed67923761
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-06 16:06:36 -0500

    redo 1

--- src/Components/FashCard/FlashCard.tsx
+++ src/Components/FashCard/FlashCard.tsx
@@ -3,67 +3,44 @@ import { ArrayMethods } from "../ArrayQuestions/ArrayMethods";
 import Timer from "../Timer/Timer";
 import "./FlashCardS.scss";
 import { Button, Card, CardGroup } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useRandomQuestion } from "../Utils/useRandomQuestion";
 import { MyQuestionContext } from "../QuestionContext/QuestionContext";
 import { Loader } from "@aws-amplify/ui-react";
-import { Question } from "../../Apis/types";
-
-const getQuestions = (
-    questions: Question[],
-    questionId: string | undefined
-): Question | undefined => {
-    return questions?.find(({ id }) => id === questionId);
-};
+import { Paths, Question, pathGenerator } from "../../Apis/types";
 
 const FlashCard = () => {
-    const { questionId } = useParams();
-    const [didUpdate, setDidUpdate] = useState<boolean>(false);
-    // TODO add next question functionality
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const randomQuestion = useRandomQuestion();
     const { questions } = useContext(MyQuestionContext);
-    let currentQuestion = getQuestions(questions, questionId);
+    const randomId = Math.floor(Math.random() * questions.length + 1);
+    const navigate = useNavigate();
+    const currentQuestion = questions[randomId];
 
-    if (!currentQuestion) {
-        return (
-            <h2>
-                <Loader />
-            </h2>
-        );
+    if (questions.length === 0) {
+        return <Loader />;
     } else {
         return (
-            <Card>
-                <div className="FlashCardHeader">
-                    <Card.Header className="cardHeader">
-                        <div className="FlashCardHeader-timer">
-                            <Timer />
-                        </div>
-
-                        <CardGroup className="FlashCardHeader-buttons">
-                            <Button type="button" variant="secondary">
-                                Submit
-                            </Button>
-                        </CardGroup>
+            <>
+                <Card>
+                    <Card.Header>
+                        <Timer />
                     </Card.Header>
-                </div>
-
-                <ArrayMethods cardQuestion={randomQuestion} />
-                <div className="FlashCardFooter">
-                    <Card.Footer className="cardFooter">
+                    <Card.Body>
+                        <ArrayMethods cardQuestion={currentQuestion} />
+                    </Card.Body>
+                    <Card.Footer>
                         <Button
-                            type="button"
-                            variant="secondary"
                             onClick={(e) => {
-                                currentQuestion = randomQuestion;
-                                setDidUpdate(!didUpdate);
+                                const path = pathGenerator[Paths.question](
+                                    questions[randomId].id
+                                );
+                                navigate(path);
                             }}
                         >
                             Next Question
                         </Button>
                     </Card.Footer>
-                </div>
-            </Card>
+                </Card>
+            </>
         );
     }
 };
