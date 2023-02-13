499344a05e1faf9d32e0ed4ea1d244e11c71997b
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-13 15:46:32 -0500

    version 3 of QuesitonEdit refactor/ redo

--- src/Components/QuestionEdit/QuestionEdit.tsx
+++ src/Components/QuestionEdit/QuestionEdit.tsx
@@ -1,52 +1,67 @@
-import { useContext, useState } from "react";
-import { Form, useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import {
+    Badge,
+    Button,
+    ButtonGroup,
+    Form,
+    FormLabel,
+    ListGroup,
+} from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
 import { MyQuestionContext } from "../QuestionContext/QuestionContext";
 import { Paths, Question } from "../../Apis/types";
-import { Button, FormLabel } from "react-bootstrap";
-import { Loading } from "../Loading/Loading";
-import { DifficultySelect } from "../QuestionList/QuestionFilter/DifficultySelect/DifficultySelect";
-import { FormTextArea } from "../AddQuestionForm/FormTextArea/FormTextArea";
-import { QuestionFormAnswers } from "../AddQuestionForm/QuestionFormAnswers/QuestionFormAnswers";
-import { FormInput } from "../AddQuestionForm/FromInput/FormInput";
-import { TagsSelect } from "../AddQuestionForm/TagsSelect/TagsSelect";
 import { QuestionFormBody } from "../AddQuestionForm/QuestionFormBody/QuestionFormBody";
+import { Loading } from "../Loading/Loading";
+import { ListFormat } from "typescript";
 
 export const QuestionEdit = () => {
     const { questionId } = useParams();
     const { questions, addQuestion } = useContext(MyQuestionContext);
-    const navigate = useNavigate();
-    const [questionEditValues, setQuestionEditValues] = useState<
+    const [selectedQuestion, setSelectedQuestion] = useState<
         Question | undefined
     >(questions?.find(({ id }) => id === questionId));
+    const navigate = useNavigate();
 
-    if (!questionEditValues) {
+    if (!selectedQuestion) {
         return <Loading />;
     }
 
     return (
         <Form
-            onSubmit={(e) => {
-                e.preventDefault();
-                addQuestion(questionEditValues);
-                navigate(Paths.questionList);
+            onSubmit={() => {
+                addQuestion(selectedQuestion);
             }}
         >
-            <FormLabel>
-                <h1>Question Edit</h1>
-            </FormLabel>
-            <QuestionFormBody
-                value={questionEditValues}
-                setValue={setQuestionEditValues}
-            />
-
-            <Button
-                onClick={() => {
-                    navigate(Paths.questionList);
-                }}
-            >
-                Cancel
-            </Button>
-            <Button type="submit">Save Changes</Button>
+            <ListGroup>
+                <ListGroup.Item>
+                    <FormLabel active>
+                        <Badge pill>
+                            <h1>Question Edit From</h1>
+                        </Badge>
+                    </FormLabel>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <QuestionFormBody
+                        value={selectedQuestion}
+                        setValue={setSelectedQuestion}
+                    />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <ButtonGroup>
+                        <Button variant="outline-primary" type="submit">
+                            Save
+                        </Button>
+                        <Button
+                            variant="outline-primary"
+                            onClick={() => {
+                                navigate(Paths.questionList);
+                            }}
+                        >
+                            Cancel
+                        </Button>
+                    </ButtonGroup>
+                </ListGroup.Item>
+            </ListGroup>
         </Form>
     );
 };
