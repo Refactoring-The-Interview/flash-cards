1cd3f5e1d75acfcfd6fd9f698c83aad1e4e0c765
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-14 13:45:04 -0500

    profile edit form and card remake

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,55 +1,65 @@
-import { Card, Figure, Modal } from "react-bootstrap";
+import { Badge, Button, Card, Figure, Form, FormGroup } from "react-bootstrap";
 import { StorageKey, useLocalStorage } from "../LocalStorage/LocalStorage";
-import { ConfirmButton } from "../ConfirmButton/ConfirmButton";
-import { UserInfoEditForm } from "./UserInfoEditForm/UserInfoEditForm";
-import { useState } from "react";
-import { userInfo } from "../../Apis/types";
-import { ProgressBars } from "../QuestionList/ProgressBars/ProgressBars";
 import duck from "../../Assets/IMGS/Mallard2.jpeg";
+import { UserInfoEditForm } from "./UserInfoEditForm/UserInfoEditForm";
+import { userInfo } from "../../Apis/types";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 export const Profile = () => {
     const [userInfo, setUserInfo] = useLocalStorage(StorageKey.userInfo, {
         email: "",
         password: "",
     });
-
     const [newUserInfo, setNewUserInfo] = useState<userInfo>(userInfo);
+    const [showEdit, setShowEdit] = useState<boolean>(false);
+
+    const { email, password } = userInfo;
+
+    const logout = () =>
+        setUserInfo({
+            email: "",
+            password: "",
+        });
 
     return (
         <>
             <Card>
-                <Figure>
-                    <Figure.Image
-                        width={171}
-                        height={180}
-                        alt="171x180"
-                        src={duck}
-                    />
-                </Figure>
+                <Card.Header>
+                    <Figure>
+                        <Figure.Image src={duck} width={171} height={180} />
+                    </Figure>
+                    <Badge>
+                        <h2>{email}</h2>
+                    </Badge>
+                </Card.Header>
+
                 <Card.Body>
-                    <ProgressBars />
-
-                    <Card.Text>Email: {userInfo.email}</Card.Text>
-
-                    <Card.Text>Password: {userInfo.password}</Card.Text>
+                    <Card.Text>Email: {email}</Card.Text>
+                    <Card.Text>Password: {password}</Card.Text>
                 </Card.Body>
 
-                <ConfirmButton
-                    variant="outline-primary"
-                    onConfirm={() => {
-                        setUserInfo(newUserInfo);
-                    }}
-                    modalBody={
-                        <>
-                            <Modal.Title>Edit User Info</Modal.Title>
-                            <UserInfoEditForm
-                                newUserInfo={newUserInfo}
-                                setNewUserInfo={setNewUserInfo}
-                            />
-                        </>
-                    }
-                    children={<>Edit User Info</>}
-                ></ConfirmButton>
+                {showEdit && (
+                    <UserInfoEditForm
+                        newUserInfo={newUserInfo}
+                        setNewUserInfo={setNewUserInfo}
+                    />
+                )}
+
+                <Card.Footer>
+                    <Button variant="outline-primary" onClick={logout}>
+                        Logout
+                    </Button>
+                    <Button
+                        variant="outline-primary"
+                        onClick={() => {
+                            setShowEdit(!showEdit);
+                            setUserInfo(newUserInfo);
+                        }}
+                    >
+                        {showEdit ? "Save" : "Edit User"}
+                    </Button>
+                </Card.Footer>
             </Card>
         </>
     );
