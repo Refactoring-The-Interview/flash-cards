03b1170003f59511ae19214d3b395788e29f828a
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-14 15:05:57 -0500

    rework of the ArrayMethods component for typing practice

--- src/Components/ArrayQuestions/ArrayMethods.tsx
+++ src/Components/ArrayQuestions/ArrayMethods.tsx
@@ -1,71 +1,38 @@
-import React, { useContext, useEffect, useState } from "react";
-import "./ArrayMethodsS.scss";
-import { Button, Card, ListGroup } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Question } from "../../Apis/types";
-import { MyQuestionContext } from "../QuestionContext/QuestionContext";
+import "./ArrayMethodsS.scss";
+import { useState } from "react";
 
 interface Props {
     cardQuestion: Question;
 }
 
 export const ArrayMethods = ({ cardQuestion }: Props) => {
-    const { setQuestions, questions } = useContext(MyQuestionContext);
+    const { question, answers, answer } = cardQuestion;
     const [isCorrect, setIsCorrect] = useState<string>("");
-    const [isDisabled, setIsDisabled] = useState<boolean>(false);
-    const { question, answer, answers } = cardQuestion;
-
-    useEffect(() => {
-        setIsDisabled(false);
-        setIsCorrect("");
-    }, [question]);
 
     return (
-        <div className="ArrayMethods">
-            <Card className="ArrayMethods-card">
-                <Card.Title className="ArrayMethods-description">
-                    {question}
-                </Card.Title>
+        <Card.Body>
+            <Card.Text>
+                <h5>{question}</h5>
+            </Card.Text>
 
-                <Card.Body>
-                    <ListGroup
-                        className={`${isCorrect} ArrayMethods-Answers `}
-                        horizontal
+            {answers.map((possibleAnswer) => {
+                return (
+                    <Button
+                        className={`ArrayMethods-Answers ${isCorrect}`}
+                        onClick={(e) => {
+                            if (possibleAnswer === answer) {
+                                setIsCorrect("success");
+                            } else {
+                                setIsCorrect("danger");
+                            }
+                        }}
                     >
-                        {answers.map((item: string, index: number) => {
-                            return (
-                                <ListGroup.Item
-                                    action
-                                    variant="light"
-                                    className="ArrayMethods-list-button"
-                                    key={index}
-                                >
-                                    <Button
-                                        type="button"
-                                        disabled={isDisabled}
-                                        key={index}
-                                        onClick={(e) => {
-                                            setIsDisabled(true);
-                                            if (item === answer) {
-                                                cardQuestion.correct = true;
-                                                // TODO tack correct, do i need to create another api to update a question? do with alex for work and data flow.
-                                                setQuestions([
-                                                    ...questions,
-                                                    cardQuestion,
-                                                ]);
-                                                setIsCorrect("success");
-                                            } else {
-                                                setIsCorrect("danger");
-                                            }
-                                        }}
-                                    >
-                                        {item}
-                                    </Button>
-                                </ListGroup.Item>
-                            );
-                        })}
-                    </ListGroup>
-                </Card.Body>
-            </Card>
-        </div>
+                        {answer}
+                    </Button>
+                );
+            })}
+        </Card.Body>
     );
 };
