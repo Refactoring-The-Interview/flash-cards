7128555ae362b2e82793d25a5c218414ac314b5b
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-15 11:54:04 -0500

    refacor of user profile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,66 +1,47 @@
-import { Badge, Button, Card, Figure, Form, FormGroup } from "react-bootstrap";
+import { Badge, Button, Card, Figure } from "react-bootstrap";
 import { StorageKey, useLocalStorage } from "../LocalStorage/LocalStorage";
 import duck from "../../Assets/IMGS/Mallard2.jpeg";
-import { UserInfoEditForm } from "./UserInfoEditForm/UserInfoEditForm";
-import { userInfo } from "../../Apis/types";
-import { useSearchParams } from "react-router-dom";
-import { useState } from "react";
 
 export const Profile = () => {
     const [userInfo, setUserInfo] = useLocalStorage(StorageKey.userInfo, {
         email: "",
         password: "",
     });
-    const [newUserInfo, setNewUserInfo] = useState<userInfo>(userInfo);
-    const [showEdit, setShowEdit] = useState<boolean>(false);
 
     const { email, password } = userInfo;
-
-    const logout = () =>
-        setUserInfo({
-            email: "",
-            password: "",
-        });
-
     return (
-        <>
-            <Card>
-                <Card.Header>
-                    <Figure>
-                        <Figure.Image src={duck} width={171} height={180} />
-                    </Figure>
+        <Card>
+            <Card.Header>
+                <Figure>
+                    <Figure.Image src={duck} width={180} height={180} />
+                </Figure>
+                <Card.Text>
                     <Badge>
-                        <h2>{email}</h2>
+                        <h3>{email}</h3>
                     </Badge>
-                </Card.Header>
+                </Card.Text>
+            </Card.Header>
 
-                <Card.Body>
-                    <Card.Text>Email: {email}</Card.Text>
-                    <Card.Text>Password: {password}</Card.Text>
-                </Card.Body>
+            <Card.Body>
+                <Card.Text>
+                    <Badge bg="secondary">
+                        <h6> User Email</h6>
+                    </Badge>
+                    : {email}
+                </Card.Text>
 
-                {showEdit && (
-                    <UserInfoEditForm
-                        newUserInfo={newUserInfo}
-                        setNewUserInfo={setNewUserInfo}
-                    />
-                )}
+                <Card.Text>
+                    <Badge bg="secondary">
+                        <h6>Password:</h6>
+                    </Badge>
+                    : {password}
+                </Card.Text>
+            </Card.Body>
 
-                <Card.Footer>
-                    <Button variant="outline-primary" onClick={logout}>
-                        Logout
-                    </Button>
-                    <Button
-                        variant="outline-primary"
-                        onClick={() => {
-                            setShowEdit(!showEdit);
-                            setUserInfo(newUserInfo);
-                        }}
-                    >
-                        {showEdit ? "Save" : "Edit User"}
-                    </Button>
-                </Card.Footer>
-            </Card>
-        </>
+            <Card.Footer>
+                <Button variant="outline-primary">Logout</Button>{" "}
+                <Button variant="outline-primary">Edit User</Button>
+            </Card.Footer>
+        </Card>
     );
 };
