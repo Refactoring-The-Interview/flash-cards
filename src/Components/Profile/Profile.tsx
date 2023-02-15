01941bec8415b741d18caceaa200324b46d9c5f3
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-15 12:04:05 -0500

    refacrtor of user profiles

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,14 +1,19 @@
 import { Badge, Button, Card, Figure } from "react-bootstrap";
 import { StorageKey, useLocalStorage } from "../LocalStorage/LocalStorage";
 import duck from "../../Assets/IMGS/Mallard2.jpeg";
+import { guestUserInfo, userInfo } from "../../Apis/types";
+import { useState } from "react";
+import { UserInfoEditForm } from "./UserInfoEditForm/UserInfoEditForm";
 
 export const Profile = () => {
+    const [show, setShow] = useState<boolean>(false);
     const [userInfo, setUserInfo] = useLocalStorage(StorageKey.userInfo, {
         email: "",
         password: "",
     });
-
+    const [guestInfo, setGuestInfo] = useState<userInfo>(userInfo);
     const { email, password } = userInfo;
+
     return (
         <Card>
             <Card.Header>
@@ -38,9 +43,28 @@ export const Profile = () => {
                 </Card.Text>
             </Card.Body>
 
+            {show && (
+                <UserInfoEditForm
+                    newUserInfo={guestInfo}
+                    setNewUserInfo={setGuestInfo}
+                />
+            )}
+
             <Card.Footer>
-                <Button variant="outline-primary">Logout</Button>{" "}
-                <Button variant="outline-primary">Edit User</Button>
+                <Button
+                    variant="outline-primary"
+                    onChange={() => {
+                        setUserInfo(guestUserInfo);
+                    }}
+                >
+                    Logout
+                </Button>{" "}
+                <Button
+                    variant="outline-primary"
+                    onClick={() => setShow(!show)}
+                >
+                    {show ? "Save" : "Edit User"}
+                </Button>
             </Card.Footer>
         </Card>
     );
