446d39ebbefe6889e2680b89de22aadf8898997f
sbrandon0432 <sbrandon0432@yahoo.com>
2023-02-28 15:04:56 -0500

    contact updated;

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,16 +1,17 @@
 import { ListGroup } from "react-bootstrap";
 import resume from "../../Assets/SamBrandonResume.pdf";
+import "./ContentS.scss";
 
 export const Contact = () => {
     return (
-        <ListGroup>
+        <ListGroup className="Contact">
             <ListGroup.Item variant="primary">Contact Info</ListGroup.Item>
-            <ListGroup.Item>Email: SamBranodn0432@yahoo.com</ListGroup.Item>
+            <ListGroup.Item>Email: SamBranodn0432@gamil.com</ListGroup.Item>
             <ListGroup.Item>Phone: +1(510)-856-6441</ListGroup.Item>
             <ListGroup.Item>Location: Knoxville, TN</ListGroup.Item>
 
-            <ListGroup.Item>
-                <iframe src={resume} title="resume"></iframe>
+            <ListGroup.Item className="resume-container">
+                <iframe src={resume} title="resume" className="resume"></iframe>
             </ListGroup.Item>
         </ListGroup>
     );
