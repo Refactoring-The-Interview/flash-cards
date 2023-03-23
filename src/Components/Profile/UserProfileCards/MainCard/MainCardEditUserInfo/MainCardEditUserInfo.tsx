93b979c2e4dad91f7b632827f513dc055afd05d0
sbrandon0432 <sbrandon0432@yahoo.com>
2023-03-23 13:07:38 -0400

    edit ueser implomented

--- src/Components/Profile/UserProfileCards/MainCard/MainCardEditUserInfo/MainCardEditUserInfo.tsx
+++ src/Components/Profile/UserProfileCards/MainCard/MainCardEditUserInfo/MainCardEditUserInfo.tsx
@@ -1,4 +1,4 @@
-import { Form } from "react-bootstrap";
+import { Form, ListGroup } from "react-bootstrap";
 import { userInfo } from "../../../../../Apis/types";
 
 interface Props {
@@ -7,5 +7,42 @@ interface Props {
 }
 
 export const MainCardEditUserInfo = ({ value, setValue }: Props) => {
-    return <Form></Form>;
+    const { email, password } = value;
+
+    return (
+        <Form>
+            <ListGroup variant="flush">
+                <ListGroup.Item></ListGroup.Item>
+                <ListGroup.Item>
+                    <h5>
+                        <input
+                            value={email}
+                            onChange={(e) => {
+                                const email = e.target.value;
+                                setValue({
+                                    ...value,
+                                    email,
+                                });
+                            }}
+                        />
+                    </h5>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                    <h5>
+                        <input
+                            value={password}
+                            onChange={(e) => {
+                                const password = e.target.value;
+                                setValue({
+                                    ...value,
+                                    password,
+                                });
+                            }}
+                        />
+                    </h5>
+                </ListGroup.Item>
+            </ListGroup>
+        </Form>
+    );
 };
