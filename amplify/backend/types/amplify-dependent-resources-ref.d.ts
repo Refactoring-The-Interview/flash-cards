9a9f1e15c08d0106c9a37b428119f5d80d0899ea
sbrandon0432 <sbrandon0432@yahoo.com>
2023-04-17 11:08:31 -0400

    amplify changes " ;

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,4 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
             Region: "string";
         };
     };
+    hosting: {
+        S3AndCloudFront: {
+            HostingBucketName: "string";
+            Region: "string";
+            S3BucketSecureURL: "string";
+            WebsiteURL: "string";
+        };
+    };
 };
