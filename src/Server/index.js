d20d49b8ef5435f8cc055c2a21d7a8ff96e5f200
sbrandon0432 <sbrandon0432@yahoo.com>
2023-05-19 15:11:33 -0400

    server heroku fix

--- src/Server/index.js
+++ src/Server/index.js
@@ -6,10 +6,12 @@ const {
 } = require("./DataBase");
 const express = require("express");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const { createProxyMiddleware } = require("http-proxy-middleware");
+app.use(createProxyMiddleware("/api/**", { target: "http://localhost:5000" }));
 
 app.post("/addToDataBase", (req, res) => {
     dbSetQuestion(req.body.question, (data, error) => {
